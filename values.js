91c4076fd9d98e30e148ef98627c9a7ec702406d
charukeshpanjala007 <charukesh.panjala.19.1@mountblue.tech>
2022-03-17 12:49:45 +0530

    updated values.js

--- values.js
+++ values.js
@@ -3,7 +3,7 @@ const values = function (obj) {
     if (obj && typeof (obj) === "object" && !Array.isArray(obj)) {
         for (let key in obj) {
             let value = obj[key]
-            value && typeof (value) !== "function" && output.push(value)
+            value  && output.push(value)
         }
     }
     return output
